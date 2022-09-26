87967b9190071241b64643ba5cf676d59b3004e9
Miguel Soriano <originalsorianom@gmail.com>
2022-09-25 21:42:30 -0700

    file update:

---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import coverImage from "../../assets/cover/IMG_4721.jpg";
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+         
+          <h1>
+          Hi, My name is Miguel Soriano.
+          </h1>
+
+          <h3>list of projects</h3>
+          <h3 > <a href="https://m-soriano.github.io/run-buddy/"> Runbuddy</a></h3>
+          <h3 > <a href="https://triciaax.github.io/Wine-and-Dine/"> Wine-and-Dine</a></h3>
+
+
+         
+
+          
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
